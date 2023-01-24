45b61f3834493d24b04ecac14a61486acecec202
Simón A Azócar <saimn.azocar@gmail.com>
2023-01-24 18:06:36 -0400

    useNavigate hook to go back

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,15 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./blogdata";
 
 const BlogPost = () => {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const blogpost = blogdata.find(post => post.slug === slug);
+
+  const returnToBlog = () => {
+    navigate('/blog');
+  }
 
   return (
     <>
       <h2>{blogpost.title}</h2>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
+
+      <button onClick={returnToBlog}>Volver a Blog</button>
     </>
   );
 }
